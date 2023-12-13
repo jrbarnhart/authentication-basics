aed9162b7e2d3b9792fdeb4bb9def2adfe535f0c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-13 13:07:53 -0500

    Add model for users

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ const mongoDb = process.env.MONGODB_CONNECTION;
 mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
