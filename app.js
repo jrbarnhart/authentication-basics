8066cd5d3b4bf5aa0b4bc4b1aae0f2fc950577de
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-13 13:05:46 -0500

    Create mongodb connection

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const mongoDb = process.env.MONGODB_CONNECTION;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
