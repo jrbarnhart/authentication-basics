5a69b04cbc5413c4a3a433309eb1ef20e31705e8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-13 13:13:20 -0500

    Create and set up express app

--- app.js
+++ app.js
@@ -18,3 +18,16 @@ const User = mongoose.model(
     password: { type: String, required: true },
   })
 );
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
