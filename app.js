9336c34f34baa1025514a4e4344c59e040d85948
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-13 12:32:52 -0500

    Import required dependencies

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
