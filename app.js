631f397a84e904ed56e282a347e15c6632f224c8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-17 13:51:15 -0500

    Add missing await

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
