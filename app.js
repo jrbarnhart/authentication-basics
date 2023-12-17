4461f2cc1fe3628ef7dc114f88474741a0b6202f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-17 12:54:48 -0500

    Add fns for serializing and deserializing users for sessions

--- app.js
+++ app.js
@@ -41,6 +41,19 @@ passport.use(
   })
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
