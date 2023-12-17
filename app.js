fe963f28d6ec1b62486164876139a59aba1c5a4b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-17 13:15:46 -0500

    Add middleware for adding user to res locals

--- app.js
+++ app.js
@@ -59,7 +59,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
   try {
